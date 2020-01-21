eb99e671308b397f93c36c5fef5178c8ebb73788
swarnim0308 <42335944+swarnim0308@users.noreply.github.com>
2020-01-21 19:37:49 +0530

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,77 @@
+var prev_q = "";
+var prev_a = "";
+
+$(function() {
+  var INDEX = 0; 
+  $("#chat-submit").click(function(e) {
+    e.preventDefault();
+    var msg = $("#chat-input").val(); 
+    if(msg.trim() == ''){
+      return false;
+    }
+    generate_message(msg, 'self',"<img src='https://bootdey.com/img/Content/avatar/avatar2.png'>");
+    var buttons = [
+        {
+          name: 'Existing User',
+          value: 'existing'
+        },
+        {
+          name: 'New User',
+          value: 'new'
+        }
+      ];
+    setTimeout(function() {
+    	$.post("/",{"ques":msg,"prev_q":prev_q,"prev_a":prev_a},function(data, status){
+          prev_a = data;
+          prev_q = msg;
+          var ms = {
+          username:'bot',
+          text: prev_a,
+          ago : ''
+          };
+          answer = ms.text
+          
+      generate_message(answer, 'user',"<img src='https://cdn.mee6.xyz/assets/logo.png'>");  
+        });
+
+    }, 1000)
+    
+  })
+  
+  function generate_message(msg, type,avatar) {
+    INDEX++;
+    var str="";
+    str += "<div id='cm-msg-"+INDEX+"' class=\"chat-msg "+type+"\">";
+    str += "          <span class=\"msg-avatar\">";
+    str += avatar;
+    str += "          <\/span>";
+    str += "          <div class=\"cm-msg-text\">";
+    str += msg;
+    str += "          <\/div>";
+    str += "        <\/div>";
+    $(".chat-logs").append(str);
+    $("#cm-msg-"+INDEX).hide().fadeIn(300);
+    if(type == 'self'){
+     $("#chat-input").val(''); 
+    }    
+    $(".chat-logs").stop().animate({ scrollTop: $(".chat-logs")[0].scrollHeight}, 1000);    
+  }  
+  
+  $(document).delegate(".chat-btn", "click", function() {
+    var value = $(this).attr("chat-value");
+    var name = $(this).html();
+    $("#chat-input").attr("disabled", false);
+    generate_message(name, 'self');
+  })
+  
+  $("#chat-circle").click(function() {    
+    $("#chat-circle").toggle('scale');
+    $(".chat-box").toggle('scale');
+  })
+  
+  $(".chat-box-toggle").click(function() {
+    $("#chat-circle").toggle('scale');
+    $(".chat-box").toggle('scale');
+  })
+  
+})
